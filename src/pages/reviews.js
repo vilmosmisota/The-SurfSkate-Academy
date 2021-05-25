6001889398eaa311fccf90c7fc4c178b52fb5e2a
Vilmos Misota <misotavilmos@gmail.com>
2021-05-25 23:10:15 +0100

    start review page

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -2,9 +2,20 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const reviews = () => {
+  const myStyle = {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }
   return (
     <Layout>
-      <h1>reviews</h1>
+      <div style={myStyle}>
+        <h1 style={{ textTransform: "capitalize" }}>
+          Ooops, I am still working on this page... come back later
+        </h1>
+      </div>
     </Layout>
   )
 }
