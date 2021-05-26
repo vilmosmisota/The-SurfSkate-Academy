510f1dd2d9f0733138ee23393fdacc7f838b3894
Vilmos Misota <misotavilmos@gmail.com>
2021-05-26 22:59:21 +0100

    bug

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,22 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 const error = () => {
+  const myStyle = {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }
   return (
-    <div>
-      <h1>Page doesn't exist</h1>
-    </div>
+    <Layout>
+      <div style={myStyle}>
+        <h1 style={{ textTransform: "capitalize" }}>
+          Ooops, No page found. 404 error.
+        </h1>
+      </div>
+    </Layout>
   )
 }
 
