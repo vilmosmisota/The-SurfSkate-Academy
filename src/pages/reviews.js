a97eb23fdd33cc52b962ed7d8b01a3b06481b938
Vilmos Misota <misotavilmos@gmail.com>
2021-06-03 21:50:59 +0100

    add review page

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -1,21 +1,32 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Seo from "../components/Seo"
 
 const reviews = () => {
-  const myStyle = {
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  }
   return (
     <Layout>
-      <div style={myStyle}>
-        <h1 style={{ textTransform: "capitalize" }}>
-          Ooops, I am still working on this page... come back later
+      <Seo title="Review" description="Review me on Trustpilot" />
+      <main className="contact-page">
+        <h1>
+          To support my business and spread the Surfskate word, please review me
+          on Trustpilot. Click to the "REVIEW" below.
         </h1>
-      </div>
+        <p>
+          hope you enjoy your time at The Surskate Academy and please let me
+          know how your surfing sessions go afterwards by pinging me a DM, email
+          or dropping me a comment on Instagram
+        </p>
+
+        <a
+          href="https://uk.trustpilot.com/evaluate/thesurfskateacademy.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div>
+            <p>REVIEW</p>
+          </div>
+        </a>
+      </main>
     </Layout>
   )
 }
